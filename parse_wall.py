5a4fc1de3951eccf5a270fab750ee03e5fc9ac24
ivanovsergey <ivanovserg990@gmail.com>
2015-02-06 18:51:52 +0800

    Find end of all posts; extract posts from the wall

--- parse_wall.py
+++ parse_wall.py
@@ -2,7 +2,7 @@
 from __future__ import division
 import os
 
-def find_beginning(text):
+def find_beginning_of_posts(text):
     flag1 = False
     idx = 0
 
@@ -15,16 +15,59 @@ def find_beginning(text):
         idx += 1
     return idx
 
+def find_end_of_posts(text, start):
+    idx = 0
+    for line in text[start:]:
+        if line.startswith(" 1."):
+            break
+        idx += 1
+    return start + idx
+
 def find_end_of_next_post(text, beginning):
     idx = 0
-    while True:
-        idx += 1
-        line = text[beginning+idx]
+    for line in text[beginning+1:]:
         if line.startswith("    <#>"):
             break
-    return beginning+idx
+        idx += 1
+    return beginning + idx + 1
 
-#TODO find end of posts
+def extract_posts(text):
+    start = find_beginning_of_posts(text)
+    finish = find_end_of_posts(text, start)
+    it = 0
+    posts = []
+
+    start_post = start
+    end_post = find_end_of_next_post(text, start_post)
+    post = text[start_post:end_post+1]
+    posts.append("\n".join(post))
+
+    last_end_post = end_post # sanity check
+    while True:
+        it += 1
+        start_post = end_post
+        end_post = find_end_of_next_post(text, start_post)
+
+        # check that we don't stuck
+        if last_end_post == end_post:
+            break
+        else:
+            last_end_post = end_post
+        # check until we reach finish line
+        if end_post < finish:
+            post = text[start_post:end_post+1]
+            posts.append("\n".join(post))
+        else:
+            post = text[start_post:finish]
+            posts.append("\n".join(post))
+            break
+    return posts
+
+
+
+
+
+
 
 if __name__ == "__main__":
 
@@ -35,12 +78,19 @@ if __name__ == "__main__":
             text.append(line.split(os.linesep)[0])
     # for i, line in enumerate(text):
     #     print i, line[:20]
-    beginning = find_beginning(text)
-    end = find_end_of_next_post(text, beginning)
-    print beginning
-    print end
-    print text[beginning]
-    print text[beginning:end]
+    start= find_beginning_of_posts(text)
+    finish = find_end_of_posts(text, start)
+    end = find_end_of_next_post(text, start)
+    # print start
+    # print finish
+    # print end
+    # print text[finish]
+    # print '\n'.join(text[start:end])
+    posts = extract_posts(text)
+    for post in posts:
+        print post
+        print '*************************'
+        print
 
 
     console = []
